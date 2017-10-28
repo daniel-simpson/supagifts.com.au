f18ebebfed2737cc44aa1adb123bfca11784fcb8
Daniel Simpson <daniel_simpson1988@hotmail.com>
2017-10-28 14:37:40 +1100

    Add padding to ensure footer doesn't have under-content

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -97,7 +97,7 @@ class TemplateWrapper extends React.PureComponent {
           style={{
             margin: "0 auto",
             maxWidth: 960,
-            padding: "0px 1.0875rem 1.45rem",
+            padding: "0px 1.0875rem 5rem",
             paddingTop: 0
           }}
         >
