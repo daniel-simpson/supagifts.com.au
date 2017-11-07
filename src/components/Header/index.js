f55dddc47e7d099512ae06260c417ce8b4f7417a
Daniel Simpson <daniel.simpson@deepend.com.au>
2017-11-07 15:33:37 +1100

    Update header to allow for subitems and style updates

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import Headroom from "react-headroom";
 
 import DropdownMenu from "../DropdownMenu";
 
-import "./header.scss";
+import style from "./header.module.scss";
 
 class Header extends React.PureComponent {
   static propTypes = {
@@ -33,50 +33,21 @@ class Header extends React.PureComponent {
     return (
       <div>
         <Headroom>
-          <header className="header">
+          <header className={style.header}>
             <div>
-              <Link
-                to="/"
-                style={{
-                  color: "white",
-                  textDecoration: "none",
-                  margin: 0
-                }}
-              >
-                <h2
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    marginBottom: "0"
-                  }}
-                >
+              <Link to="/" className={style.home_link}>
+                <h2>
                   <img
                     alt="Supagifts"
                     src="/assets/images/logo_and_tagline.png"
-                    style={{
-                      height: "6rem",
-                      marginBottom: "0"
-                    }}
                   />
                 </h2>
               </Link>
 
-              <nav className="header_navigation">
-                <ul
-                  style={{
-                    marginBottom: "0"
-                  }}
-                >
+              <nav className={style.header_navigation}>
+                <ul>
                   {menuItems.map(m => (
-                    <li
-                      key={m.id}
-                      style={{
-                        display: "inline-block",
-                        marginRight: "1rem",
-                        marginBottom: "0"
-                      }}
-                    >
+                    <li key={m.id}>
                       {m.children && m.children.length ? (
                         <DropdownMenu data={m} />
                       ) : (
@@ -88,7 +59,7 @@ class Header extends React.PureComponent {
               </nav>
 
               <div
-                className="mobile_nav__trigger_open"
+                className={style.mobile_nav__trigger_open}
                 onClick={this.showMobileNav.bind(this)}
               >
                 Menu
@@ -98,50 +69,43 @@ class Header extends React.PureComponent {
         </Headroom>
 
         <div
-          className="mobile_nav_wrapper"
+          className={style.mobile_nav_wrapper}
           style={{
             display: this.state.mobileMenuOpen ? "" : "none"
           }}
           onClick={this.hideMobileNav.bind(this)}
         >
           <nav
-            className="mobile_navigation"
+            className={style.mobile_navigation}
             onClick={e => {
               e.stopPropagation();
             }}
           >
             <a
-              className="mobile_nav__trigger_close"
+              className={style.mobile_nav__trigger_close}
               onClick={this.hideMobileNav.bind(this)}
             >
               Close
             </a>
             <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
               {menuItems.map(m => (
-                <li
-                  key={m.id}
-                  style={{
-                    marginRight: "1rem",
-                    marginBottom: "0"
-                  }}
-                >
+                <li key={m.id}>
                   {m.children && m.children.length > 0 ? (
-                    <ul>
-                      {m.children.map(n => {
-                        <li key={n.id}>
-                          <Link
-                            to={n.slug}
-                            onClick={this.hideMobileNav.bind(this)}
-                          >
-                            {n.title}
-                          </Link>
-                        </li>;
-                      })}
-                    </ul>
+                    <div>
+                      <span>{m.title}</span>
+                      <ul className={style.mobile_submenu}>
+                        {m.children.map(n => (
+                          <li key={n.id}>
+                            <Link to={n.slug}>{n.title}</Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
                   ) : (
-                    <Link to={m.slug} onClick={this.hideMobileNav.bind(this)}>
-                      {m.title}
-                    </Link>
+                    <Link to={m.slug}>{m.title}</Link>
                   )}
                 </li>
               ))}
