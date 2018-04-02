e63660b89aace70b828a4d97dc6290d1037c2c97
Daniel Simpson <daniel_simpson1988@hotmail.com>
2018-04-02 18:45:09 +1000

    Paperform updates

--- src/components/PaperformEmbed/index.js
+++ src/components/PaperformEmbed/index.js
@@ -1,21 +1,28 @@
 import React from "react";
 import Link from "gatsby-link";
 import * as PropTypes from "prop-types";
-import Helmet from "react-helmet";
 
 class PaperformEmbed extends React.PureComponent {
   static propTypes = {
     id: PropTypes.string.isRequired
   };
 
+  componentDidMount() {
+    if (!document.querySelectorAll("script[src='https://paperform.co/__embed']").length) {
+
+      const paperformEmbed = document.createElement("script");
+      paperformEmbed.src = "https://paperform.co/__embed"
+      paperformEmbed.async = true;
+
+      document.body.appendChild(paperformEmbed);
+    }
+  }
+
   render() {
     const id = this.props.id;
 
     return (
       <div>
-        <Helmet>
-          <script src="https://paperform.co/__embed" />
-        </Helmet>
         <div data-paperform-id={id} />
       </div>
     );
