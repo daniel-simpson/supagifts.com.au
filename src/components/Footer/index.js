dcc39982ee05cb7cd342f5a7a488f15f7ae1e73a
Daniel Simpson <daniel_simpson1988@hotmail.com>
2018-04-19 12:54:44 +1000

    Fix up footer styles, remove unnecessary body stuff

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Link from "gatsby-link";
 
-import "./footer.scss";
+import style from "./footer.module.scss";
 
 class Footer extends React.PureComponent {
   static propTypes = {
@@ -13,21 +13,23 @@ class Footer extends React.PureComponent {
     const { menuItems } = this.props;
 
     return (
-      <footer className="footer">
-        <ul>
-          {menuItems.map(m => (
-            <li key={m.id}>
-              {m.slug ? (
-                <Link to={m.slug}>{m.title}</Link>
-              ) : (
-                <a href={m.url} target="_blank">
-                  {m.title}
-                </a>
-              )}
-            </li>
-          ))}
-        </ul>
-      </footer>
+      <div className={style.footer_wrapper}>
+        <footer className={`${style.footer} footer`}>
+          <ul>
+            {menuItems.map(m => (
+              <li key={m.id}>
+                {m.slug ? (
+                  <Link to={m.slug}>{m.title}</Link>
+                ) : (
+                  <a href={m.url} target="_blank">
+                    {m.title}
+                  </a>
+                )}
+              </li>
+            ))}
+          </ul>
+        </footer>
+      </div>
     );
   }
 }
