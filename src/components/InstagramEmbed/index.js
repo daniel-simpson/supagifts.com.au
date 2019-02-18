35252bb271dea20113b6f4a88f6253e42a5c437e
Daniel Simpson <daniel_simpson1988@hotmail.com>
2019-02-19 08:54:38 +1100

    Clean up instagram stuff

--- src/components/InstagramEmbed/index.js
+++ src/components/InstagramEmbed/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { StaticQuery, graphql } from "gatsby";
 
-import { cleanCaption } from "../../utils/textUtils";
-
 import instaStyle from "./instagram.module.scss";
 
 const Instagram = ({ data }) => {
@@ -14,7 +12,6 @@ const Instagram = ({ data }) => {
       <ul className={instaStyle.container}>
         {tiles.map(tile => {
           const thumbnail = tile.thumbnails[tile.thumbnails.length - 2];
-          // <p>{cleanCaption(tile.caption)}</p>
           return (
             <li key={tile.id}>
               <a
